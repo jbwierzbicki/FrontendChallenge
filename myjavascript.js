3a5778d7d1067651b2cc57821675ba6921b17bdc
Johnathan <jbwierzbicki@gmail.com>
2018-12-27 16:33:58 -0500

    [added] prototype reconstruction x3

--- myjavascript.js
+++ myjavascript.js
@@ -1,3 +1,5 @@
+
+//Timer backend for bus listings
 function startTimer(duration, display) {
   var start = Date.now(),
     diff,
@@ -34,6 +36,7 @@ function startTimer(duration, display) {
   setInterval(timer, 1000);
 };
 
+
 window.onload = function() {
   clock();
   //Live 12 hour time for status bar
@@ -67,10 +70,7 @@ window.onload = function() {
     document.getElementById('bus-counter').innerHTML = busListingCount;
   }
 
-  //Timer function for bus listings
-
-
-
+  //Timer functions for bus listings
   listing1Timer();
   function listing1Timer() {
     var countdownTime = 60 * 20,
@@ -103,4 +103,162 @@ window.onload = function() {
       window.setInterval(startTimer, countdownTime);
   }
 
+  listing5Timer();
+  function listing5Timer() {
+    var countdownTime = 60 * 10,
+      display = document.getElementById('listing-5-timer');
+      startTimer(countdownTime, display);
+      window.setInterval(startTimer, countdownTime);
+  }
+
+}
+
+//Modal box generator
+function busListing1() {
+  var mainScreenBusNumber = document.getElementById('bus-number-1');
+  var modalScreenBusNumber = document.getElementById('modal-bus-number');
+  modalScreenBusNumber.innerHTML = mainScreenBusNumber.innerHTML;
+
+  var mainScreenDestination = document.getElementById('bus-destination-1');
+  var modalScreenDestination = document.getElementById('modal-bus-destination');
+  modalScreenDestination.innerHTML = "Bus to " + mainScreenDestination.innerHTML;
+
+  var mainScreenArrivaltime = document.getElementById('listing-1-timer');
+  var modalScreenArrivaltime = document.getElementById('modal-bus-arrivaltime');
+  modalScreenArrivaltime.innerHTML = "Arriving in " + mainScreenArrivaltime.innerHTML + " - Headed East";
+
+  document.getElementById('selected-route-container').style.display = "block";
+}
+
+function busListing2() {
+  var mainScreenBusNumber = document.getElementById('bus-number-2');
+  var modalScreenBusNumber = document.getElementById('modal-bus-number');
+  modalScreenBusNumber.innerHTML = mainScreenBusNumber.innerHTML;
+
+  var mainScreenDestination = document.getElementById('bus-destination-2');
+  var modalScreenDestination = document.getElementById('modal-bus-destination');
+  modalScreenDestination.innerHTML = "Bus to " + mainScreenDestination.innerHTML;
+
+  var mainScreenArrivaltime = document.getElementById('listing-2-timer');
+  var modalScreenArrivaltime = document.getElementById('modal-bus-arrivaltime');
+  modalScreenArrivaltime.innerHTML = "Arriving in " + mainScreenArrivaltime.innerHTML + " - Headed East";
+
+  document.getElementById('selected-route-container').style.display = "block";
+}
+
+function busListing3() {
+  var mainScreenBusNumber = document.getElementById('bus-number-3');
+  var modalScreenBusNumber = document.getElementById('modal-bus-number');
+  modalScreenBusNumber.innerHTML = mainScreenBusNumber.innerHTML;
+
+  var mainScreenDestination = document.getElementById('bus-destination-3');
+  var modalScreenDestination = document.getElementById('modal-bus-destination');
+  modalScreenDestination.innerHTML = "Bus to " + mainScreenDestination.innerHTML;
+
+  var mainScreenArrivaltime = document.getElementById('listing-3-timer');
+  var modalScreenArrivaltime = document.getElementById('modal-bus-arrivaltime');
+  modalScreenArrivaltime.innerHTML = "Arriving in " + mainScreenArrivaltime.innerHTML + " - Headed North";
+
+  document.getElementById('selected-route-container').style.display = "block";
+}
+
+function busListing4() {
+  var mainScreenBusNumber = document.getElementById('bus-number-4');
+  var modalScreenBusNumber = document.getElementById('modal-bus-number');
+  modalScreenBusNumber.innerHTML = mainScreenBusNumber.innerHTML;
+
+  var mainScreenDestination = document.getElementById('bus-destination-4');
+  var modalScreenDestination = document.getElementById('modal-bus-destination');
+  modalScreenDestination.innerHTML = "Bus to " + mainScreenDestination.innerHTML;
+
+  var mainScreenArrivaltime = document.getElementById('listing-4-timer');
+  var modalScreenArrivaltime = document.getElementById('modal-bus-arrivaltime');
+  modalScreenArrivaltime.innerHTML = "Arriving in " + mainScreenArrivaltime.innerHTML + " - Headed South";
+
+  document.getElementById('selected-route-container').style.display = "block";
+}
+
+function busListing5() {
+  var mainScreenBusNumber = document.getElementById('bus-number-5');
+  var modalScreenBusNumber = document.getElementById('modal-bus-number');
+  modalScreenBusNumber.innerHTML = mainScreenBusNumber.innerHTML;
+
+  var mainScreenDestination = document.getElementById('bus-destination-5');
+  var modalScreenDestination = document.getElementById('modal-bus-destination');
+  modalScreenDestination.innerHTML = "Bus to " + mainScreenDestination.innerHTML;
+
+  var mainScreenArrivaltime = document.getElementById('listing-5-timer');
+  var modalScreenArrivaltime = document.getElementById('modal-bus-arrivaltime');
+  modalScreenArrivaltime.innerHTML = "Arriving in " + mainScreenArrivaltime.innerHTML + " - Headed South";
+
+  document.getElementById('selected-route-container').style.display = "block";
+}
+
+function busListing6() {
+  var mainScreenBusNumber = document.getElementById('bus-number-6');
+  var modalScreenBusNumber = document.getElementById('modal-bus-number');
+  modalScreenBusNumber.innerHTML = mainScreenBusNumber.innerHTML;
+
+  var mainScreenDestination = "Jackson Park Express";
+  var modalScreenDestination = document.getElementById('modal-bus-destination');
+  modalScreenDestination.innerHTML = "<span style='font-size: 15.3px;'>Bus to " + mainScreenDestination + "</span>";
+
+  var mainScreenArrivaltime = document.getElementById('listing-6-timer');
+  var modalScreenArrivaltime = document.getElementById('modal-bus-arrivaltime');
+  modalScreenArrivaltime.innerHTML = mainScreenArrivaltime.innerHTML + " - <span style='color:#B82121;'>Reroute in progress</span>";
+
+  document.getElementById('selected-route-container').style.display = "block";
+}
+
+function busListing7() {
+  var mainScreenBusNumber = document.getElementById('bus-number-7');
+  var modalScreenBusNumber = document.getElementById('modal-bus-number');
+  modalScreenBusNumber.innerHTML = mainScreenBusNumber.innerHTML;
+
+  var mainScreenDestination = document.getElementById('bus-destination-7');
+  var modalScreenDestination = document.getElementById('modal-bus-destination');
+  modalScreenDestination.innerHTML = "Bus to " + mainScreenDestination.innerHTML;
+
+  var mainScreenArrivaltime = document.getElementById('listing-7-timer');
+  var modalScreenArrivaltime = document.getElementById('modal-bus-arrivaltime');
+  modalScreenArrivaltime.innerHTML = "<span style='color:#FFD800;'>" + mainScreenArrivaltime.innerHTML + "</span> " + " Out of service until 12:10 AM";
+
+  document.getElementById('selected-route-container').style.display = "block";
+}
+
+function busListing8() {
+  var mainScreenBusNumber = document.getElementById('bus-number-8');
+  var modalScreenBusNumber = document.getElementById('modal-bus-number');
+  modalScreenBusNumber.innerHTML = mainScreenBusNumber.innerHTML;
+
+  var mainScreenDestination = document.getElementById('bus-destination-8');
+  var modalScreenDestination = document.getElementById('modal-bus-destination');
+  modalScreenDestination.innerHTML = "Bus to " + mainScreenDestination.innerHTML;
+
+  var mainScreenArrivaltime = document.getElementById('listing-8-timer');
+  var modalScreenArrivaltime = document.getElementById('modal-bus-arrivaltime');
+  modalScreenArrivaltime.innerHTML = "<span style='color:#FFD800;'>" + mainScreenArrivaltime.innerHTML + "</span> " + " Out of service until 12:10 AM";
+
+  document.getElementById('selected-route-container').style.display = "block";
+}
+
+function busListing9() {
+  var mainScreenBusNumber = document.getElementById('bus-number-9');
+  var modalScreenBusNumber = document.getElementById('modal-bus-number');
+  modalScreenBusNumber.innerHTML = mainScreenBusNumber.innerHTML;
+
+  var mainScreenDestination = document.getElementById('bus-destination-9');
+  var modalScreenDestination = document.getElementById('modal-bus-destination');
+  modalScreenDestination.innerHTML = "Bus to " + mainScreenDestination.innerHTML;
+
+  var mainScreenArrivaltime = document.getElementById('listing-9-timer');
+  var modalScreenArrivaltime = document.getElementById('modal-bus-arrivaltime');
+  modalScreenArrivaltime.innerHTML = "<span style='color:#FFD800;'>" + mainScreenArrivaltime.innerHTML + "</span> " + " Out of service until 6:00 AM";
+
+  document.getElementById('selected-route-container').style.display = "block";
+}
+
+//Close modal box
+function closeModal() {
+  document.getElementById('selected-route-container').style.display = "none";
 }
